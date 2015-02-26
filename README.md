8ec79b7a3ac862a3eda5ca8a0e050371e701e88d
hisaichi5518 <hisaichi5518@gmail.com>
2015-02-26 10:00:56 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Hash::Filter - It's new $module
+
+# SYNOPSIS
+
+    use Hash::Filter;
+
+# DESCRIPTION
+
+Hash::Filter is ...
+
+# LICENSE
+
+Copyright (C) hisaichi5518.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+hisaichi5518 <hisaichi5518@gmail.com>
---
+++ lib/Hash/Filter.pm
@@ -0,0 +1,39 @@
+package Hash::Filter;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Hash::Filter - It's new $module
+
+=head1 SYNOPSIS
+
+    use Hash::Filter;
+
+=head1 DESCRIPTION
+
+Hash::Filter is ...
+
+=head1 LICENSE
+
+Copyright (C) hisaichi5518.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+hisaichi5518 E<lt>hisaichi5518@gmail.comE<gt>
+
+=cut
+
